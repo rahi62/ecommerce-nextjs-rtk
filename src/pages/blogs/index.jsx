7ae9135033ca3e0acc3c63572970a8875390bf66
rahi62 <bakhshayesh84@gmail.com>
2023-02-01 04:56:33 +0330

    change in blog component

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -4,7 +4,7 @@ const Products = () => {
     return (
         <>
             <div className="flex justify-center items-center text-center min-h-screen">
-                <h1 className="text-2xl font-bold">All Blogs List...</h1>
+                <h1 className="text-2xl font-bold">All Blogs List</h1>
             </div>
         </>
     )
